25ef08fba65be604b31af7dea36954593ffb583c
CharlyGoros <111509721+CharlyGoros@users.noreply.github.com>
2025-05-03 17:22:15 -0300

    agregada pag de registro, falta los footer y el acceso de busqueda, arreglar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import BusquedaPage from "./pages/busqueda";
+import Registro from "./pages/registro";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<BusquedaPage />} />
+        <Route path="/registro" element={<Registro />} />
+
       </Routes>
     </Router>
   );
